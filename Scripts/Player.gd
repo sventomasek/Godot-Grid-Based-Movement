509cc9ace510726a7d03c48710e3fc08f9e463b7
Sven Tomašek <61661787+sventomasek@users.noreply.github.com>
2024-08-08 01:42:21 +0200

    Added interpolation for the Player
    - The player now has 3 interpolation options "None", "Linear" and "Move Toward"
    - Fixed a Top-Down movement bug where you could clip into a wall tile at an angle

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,6 +12,8 @@ extends Node2D
 
 @export_group("Movement")
 @export_enum("Platformer", "Top-Down") var movementType = "Platformer"
+@export_enum("None", "Linear", "Move Toward") var interpolation = "None"
+@export var interpolationSpeed := 5.0
 @export var moveDelayUp := 0.5
 @export var moveDelayDown := 0.5
 var moveDelayY_: float
@@ -19,6 +21,10 @@ var moveDelayY_: float
 @export var moveDelayLeft := 0.5
 @export var moveDelayRight := 0.5
 var moveDelayX_: float
+
+var oldPosition: Vector2
+var newPosition: Vector2
+var doneMoving := true
 
 @export_group("Jumping")
 @export var inverseGravity := false
@@ -32,7 +38,10 @@ var isOnFloor: bool
 @onready var tileMap = %TileMap ## Right click on your TileMap and select "Access as Unique name" (make sure the name is TileMap)
 
 func _ready():
-	global_position = tileMap.map_to_local(global_position)
+	var currentTile: Vector2i = tileMap.local_to_map(global_position)
+	newPosition = tileMap.map_to_local(currentTile)
+	oldPosition = newPosition
+	global_position = newPosition
 	
 func _physics_process(delta):
 	# Death
@@ -41,6 +50,9 @@ func _physics_process(delta):
 		visible = false
 		return
 		
+	# Movement
+	update_position()
+	
 	# Horizontal Movement
 	moveDelayX_ -= delta
 	var direction = Input.get_axis(moveLeft, moveRight)
@@ -48,11 +60,6 @@ func _physics_process(delta):
 	if direction > 0: move_x(1)
 	elif direction < 0: move_x(-1)
 	
-	# Flip Sprite
-	if sprite:
-		if direction > 0: sprite.flip_h = false
-		elif direction < 0: sprite.flip_h = true
-		
 	# Vertical Movement
 	moveDelayY_ -= delta
 	
@@ -77,8 +84,10 @@ func _physics_process(delta):
 		
 		if jumping: do_jumping()
 		elif !isOnFloor: falling()
+		
 	elif movementType == "Top-Down":
 		var directionY = Input.get_axis(moveDown, moveUp)
+		
 		if directionY > 0: move_y(-1)
 		elif directionY < 0: move_y(1)
 		
@@ -98,25 +107,45 @@ func falling():
 	else: move_y(1)
 	
 func move_x(direction: int):
-	if moveDelayX_ > 0: return
+	if moveDelayX_ > 0 || !doneMoving: return
 	
 	var currentTile: Vector2i = tileMap.local_to_map(global_position)
 	var targetTile := Vector2i(currentTile.x + direction, currentTile.y)
-	var tileData: TileData = tileMap.get_cell_tile_data(0, targetTile)
-	if tileData && !tileData.get_custom_data("walkable"): return
 	
-	global_position.x = tileMap.map_to_local(targetTile).x
+	oldPosition.x = tileMap.map_to_local(currentTile).x
+	newPosition.x = tileMap.map_to_local(targetTile).x
 	if direction > 0:  moveDelayX_ = moveDelayRight
 	elif direction < 0: moveDelayX_ = moveDelayLeft
 	
+	# Flip Sprite
+	if sprite:
+		if direction > 0: sprite.flip_h = false
+		elif direction < 0: sprite.flip_h = true
+		
 func move_y(direction: int):
-	if moveDelayY_ > 0: return
+	if moveDelayY_ > 0 || !doneMoving: return
 	
 	var currentTile: Vector2i = tileMap.local_to_map(global_position)
 	var targetTile := Vector2i(currentTile.x, currentTile.y + direction)
-	var tileData: TileData = tileMap.get_cell_tile_data(0, targetTile)
-	if tileData && !tileData.get_custom_data("walkable"): return
 	
-	global_position.y = tileMap.map_to_local(targetTile).y
+	oldPosition.y = tileMap.map_to_local(currentTile).y
+	newPosition.y = tileMap.map_to_local(targetTile).y
 	if direction > 0:  moveDelayY_ = moveDelayDown
 	elif direction < 0: moveDelayY_ = moveDelayUp
+	
+func update_position():
+	# Check if next position is inside wall
+	var tileData: TileData = tileMap.get_cell_tile_data(0, tileMap.local_to_map(newPosition))
+	if tileData && !tileData.get_custom_data("walkable"):
+		newPosition = oldPosition
+		return
+		
+	# Update Position
+	var delta = get_physics_process_delta_time()
+	oldPosition = newPosition
+	match interpolation:
+		"Linear": global_position = lerp(global_position, newPosition, delta * interpolationSpeed)
+		"Move Toward":
+			global_position.x = move_toward(global_position.x, newPosition.x, interpolationSpeed * 0.1)
+			global_position.y = move_toward(global_position.y, newPosition.y, interpolationSpeed * 0.1)
+		"None": global_position = newPosition
